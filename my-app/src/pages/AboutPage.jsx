9cba139cc9cbb46146d04e49b4861b8d97486f37
grok369-nexus <annonymous369jh@gmail.com>
2026-08-23 12:39:59 +0300

    Add AboutPage component with company information

---
+++ my-app/src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../components/Card";
+
+export default function AboutPage() {
+  return (
+    <div className="p-10 flex justify-center">
+      <Card title="About Us">
+        Vortex Dynamics is committed to building futuristic software solutions
+        with a consistent brand identity across websites and apps. Our mission
+        is to empower students and businesses with practical, innovative tools.
+      </Card>
+    </div>
+  );
+}
